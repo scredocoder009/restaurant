36b0a421657238ae0ff58f1bc091624c73ef82ac
scredocoder009 <scredocoder009@gmail.com>
2022-11-22 16:13:50 +0400

    fix

--- restourant/Cook.cs
+++ restourant/Cook.cs
@@ -11,37 +11,38 @@ namespace restourant
         private int chicken_quantity = 0; 
         private int egg_quantity = 0; 
         private ChickenOrder chickenOrder; 
+        private int chickenQuantity = 0;
+        private int eggQuantity = 0;
+        private ChickenOrder chickenOrder;
         private EggOrder eggOrder;
 
         public void Submit(MenuItem item)
         {
             if (item == MenuItem.chicken)
             {
-                chicken_quantity++;
-              
+                chickenQuantity++;
             }
             else if (item == MenuItem.egg)
             {
-                egg_quantity++;
-              
+                eggQuantity++;
             }
         }
 
         public void PrepareFood()
         {
-            if (chicken_quantity > 0)
+            if (chickenQuantity > 0)
             {
-                chickenOrder = new ChickenOrder(chicken_quantity);
-                for (int j = 0; j < chicken_quantity; j++)
+                chickenOrder = new ChickenOrder(chickenQuantity);
+                for (int j = 0; j < chickenQuantity; j++)
                 {
                     chickenOrder.CutUp();
                 }
                 chickenOrder.Cook();
             }
-            if (egg_quantity > 0)
+            if (eggQuantity > 0)
             {
-                eggOrder = new EggOrder(egg_quantity);
-                for (int j = 0; j < egg_quantity; j++)
+                eggOrder = new EggOrder(eggQuantity);
+                for (int j = 0; j < eggQuantity; j++)
                 {
                     eggOrder.Crack();
                     eggOrder.DiscardShell();
@@ -52,7 +53,7 @@ namespace restourant
 
         public string Inspect()
         {
-            if (egg_quantity > 0)
+            if (eggQuantity > 0)
             {
                 return eggOrder.GetQuality().ToString();
             }
